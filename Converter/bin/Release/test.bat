225f8102fa7ad7f9743eef308a8b32f42e5621fa
Karsten Bolge <karstenbolge@yahoo.dk>
2020-05-19 12:49:27 +0200

    added

---
+++ Converter/bin/Release/test.bat
@@ -0,0 +1,73 @@
+rmdir /s /q  C:\SuperPort\Output\%DATE%
+
+REM copy C:\SuperPort\Import\ACCREL.HEM.190528 "C:\SuperPort\Input\Nykredit\ACCREL.HEM.190528"
+REM copy C:\SuperPort\Import\KNTBV.HEM.190528 "C:\SuperPort\Input\Nykredit\KNTBV.HEM.190528"
+REM copy C:\SuperPort\Import\OBLUD.HEM.190528 "C:\SuperPort\Input\Nykredit\OBLUD.HEM.190528"
+REM copy C:\SuperPort\Import\PFBEH.HEM.190528 "C:\SuperPort\Input\Nykredit\PFBEH.HEM.190528"
+REM copy C:\SuperPort\Import\PFHDL.HEM.190528 "C:\SuperPort\Input\Nykredit\PFHDL.HEM.190528"
+REM copy C:\SuperPort\Import\PFKNT.HEM.190528 "C:\SuperPort\Input\Nykredit\PFKNT.HEM.190528"
+REM copy C:\SuperPort\Import\PFUDB.HEM.190528 "C:\SuperPort\Input\Nykredit\PFUDB.HEM.190528"
+
+REM copy "C:\SuperPort\Import\UBYT-050250000111111-20190415-20190417-BDCSVF3.csv" "C:\SuperPort\Input\Jyske Bank\UBYT-050250000111111-20190415-20190417-BDCSVF3.csv"
+
+REM copy C:\SuperPort\Import\P3HDL.DAT "C:\SuperPort\Input\Danske Bank\P3HDL.DAT"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_001" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_001"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_002" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_002"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_003" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_003"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_004" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_004"
+
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_007" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_007"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL-1.DAT_008" "C:\SuperPort\Input\Danske Bank\P3HDL-1.DAT_008"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL-2.DAT_009" "C:\SuperPort\Input\Danske Bank\P3HDL-2.DAT_009"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_010" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_010"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_011" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_011"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_012" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_012"
+
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL-1.DAT_013" "C:\SuperPort\Input\Danske Bank\P3HDL-1.DAT_013"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL-2.DAT_014" "C:\SuperPort\Input\Danske Bank\P3HDL-2.DAT_014"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL-3.DAT_015" "C:\SuperPort\Input\Danske Bank\P3HDL-3.DAT_015"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL-4.DAT_016" "C:\SuperPort\Input\Danske Bank\P3HDL-4.DAT_016"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL-5.DAT_017" "C:\SuperPort\Input\Danske Bank\P3HDL-5.DAT_017"
+
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL-1.DAT_018" "C:\SuperPort\Input\Danske Bank\P3HDL-1.DAT_018"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL-2.DAT_019" "C:\SuperPort\Input\Danske Bank\P3HDL-2.DAT_019"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL-3.DAT_020" "C:\SuperPort\Input\Danske Bank\P3HDL-3.DAT_020"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL-4.DAT_021" "C:\SuperPort\Input\Danske Bank\P3HDL-4.DAT_021"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL-5.DAT_022" "C:\SuperPort\Input\Danske Bank\P3HDL-5.DAT_022"
+
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_023" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_023"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_024" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_024"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_025" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_025"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_026" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_026"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_027" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_027"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_028" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_028"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_029" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_029"
+
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_035" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_035"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_036" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_036"
+REM copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_037" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_037"
+
+copy "C:\SuperPort\Danske Bank - Test data\P3HDL.DAT_065" "C:\SuperPort\Input\Danske Bank\P3HDL.DAT_065"
+
+REM copy "C:\SuperPort\Jydske Bank - Test data\UBYT-050780000034950-20190901-20190913-BDCSVF3.CSV" "C:\SuperPort\Input\Jydske Bank\UBYT-050780000034950-20190901-20190913-BDCSVF3.CSV"
+REM copy "C:\SuperPort\Jydske Bank - Test data\BEHL-050780000034950-20190916-BDCSVF3 _1_.CSV" "C:\SuperPort\Input\Jydske Bank\BEHL-050780000034950-20190916-BDCSVF3 _1_.CSV"
+REM copy "C:\SuperPort\Jydske Bank - Test data\Koeb_salg Jyske Bank.CSV" "C:\SuperPort\Input\Jydske Bank\Koeb_salg Jyske Bank.CSV"
+REM copy "C:\SuperPort\Jydske Bank - Test data\HNDL-070180000006135-20190618-20190622-BDCSVF3 (1) EUR_Jyske.CSV" "C:\SuperPort\Input\Jydske Bank\HNDL-070180000006135-20190618-20190622-BDCSVF3 (1) EUR_Jyske.CSV"
+
+REM copy "C:\SuperPort\Jydske Bank - Test data\HNDL-MULTI-20191014-20191014-BDCSVF3.CSV" "C:\SuperPort\Input\Jydske Bank\HNDL-MULTI-20191014-20191014-BDCSVF3.CSV"
+
+REM copy "C:\SuperPort\Sydbank - Test data\BEHL-MULTI-20190916-BDCSVF3 _2_-sydbank.CSV" "C:\SuperPort\Input\Sydbank\BEHL-MULTI-20190916-BDCSVF3 _2_-sydbank.CSV"
+REM copy "C:\SuperPort\Sydbank - Test data\UBYT-MULTI-20190901-20190915-BDCSVF3-sydbank.CSV" "C:\SuperPort\Input\Sydbank\UBYT-MULTI-20190901-20190915-BDCSVF3-sydbank_.CSV"
+
+REM copy "C:\SuperPort\Jydske Bank - Test data\HNDL-MULTI-20191106-20191106-BDCSVF3.CSV" "C:\SuperPort\Input\Jydske Bank\HNDL-MULTI-20191106-20191106-BDCSVF3.CSV"
+REM copy "C:\SuperPort\Jydske Bank - Test data\HNDL-MULTI-20191108-20191110-BDCSVF3.CSV" "C:\SuperPort\Input\Jydske Bank\HNDL-MULTI-20191108-20191110-BDCSVF3.CSV"
+REM copy "C:\SuperPort\Jydske Bank - Test data\UBYT-MULTI-20191108-20191110-BDCSVF3.CSV" "C:\SuperPort\Input\Jydske Bank\UBYT-MULTI-20191108-20191110-BDCSVF3.CSV"
+
+REM copy "C:\SuperPort\Jydske Bank - Test data\UBYT-MULTI-20191101-20191103-BDCSVF3.CSV" "C:\SuperPort\Input\Jydske Bank\UBYT-MULTI-20191101-20191103-BDCSVF3.CSV"
+REM copy "C:\SuperPort\Jydske Bank - Test data\HNDL-MULTI-00010101-20191118-BDCSVF3.CSV" "C:\SuperPort\Input\Jydske Bank\HNDL-MULTI-00010101-20191118-BDCSVF3.CSV"
+
+
+C:\SuperPort\Converter\Converter\Converter\bin\Release\converter.exe C:\SuperPort\Input C:\SuperPort\Output smtp.speednames.com 1025 kub@builtit.dk Freja2018 false karstenbolge@yahoo.dk "C:\SuperPort\Fondskode.CSV" true
+
+start notepad C:\SuperPort\Output\%DATE%\log.txt
